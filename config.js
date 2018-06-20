3c6bacc452e6f117c8d226ba7739e306631aae44
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-20 18:23:28 +0200

    Added twit module and config.js
    Not working yet...

---
+++ config.js
@@ -0,0 +1,11 @@
+var Twit = require('twit')
+
+var TwitConfig = new Twit({
+  consumer_key:         'WFrrfR7cjBZeAKcsY0wopTHBE',
+  consumer_secret:      'wdndG4i1V4kVNw8Y9ShENz26Ib85L6dUxBhbHiC09KdaIekqYF',
+  access_token:         '3220983761-IGErWxZqoLH13yBDn7d4npr10JXZqGgO1NV4zKC',
+  access_token_secret:  'LBICpXl2Lnz8sEQc8BCSc3hn95aoNvDoigVOFkAQiDxqV'
+});
+
+module.exports = TwitConfig;
+
