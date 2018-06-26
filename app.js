1830255de8e38336aba31e42df976fd2ff04b133
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-06-26 22:14:28 +0200

    5 latest tweet
    Populate and display in console 5 latests tweet

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +25,16 @@ app.use('/users', usersRouter);
 
 // app.use(config.get());
 
-// get latest tweet
-config.get('statuses/user_timeline', function(err, data, response) {
-  console.log(data);
+// get 5 latest tweet
+config.get('statuses/user_timeline.json?count=5', function(err, data, response) {
+  for(let i = 0; i < data.length; i++) {
+    console.log(data[i].created_at);
+    console.log(data[i].user.name);
+    console.log(data[i].user.screen_name);
+    console.log(data[i].text);
+    console.log(data[i].favorite_count);
+    console.log(data[i].retweet_count);
+  }
 });
 
 // catch 404 and forward to error handler
