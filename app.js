195d4a8b3b8ff03fb14cabee7c02b74f7487f015
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-29 16:17:27 +0200

    modify lot of things
    added logic and api call to index.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const indexRoutes = require('./routes/index');
+const usersRoutes = require('./routes/users');
+const Twit = require('twit');
 const config = require('./config');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,44 +11,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// app.use(config.get());
-
-// get 5 latest tweet
-config.get('statuses/user_timeline.json?count=5', function(err, data, response) {
-  for(let i = 0; i < data.length; i++) {
-    console.log(data[i].created_at);
-    console.log(data[i].user.name);
-    console.log(data[i].user.screen_name);
-    console.log(data[i].text);
-    console.log(data[i].favorite_count);
-    console.log(data[i].retweet_count);
-  }
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.use('/', indexRoutes);
+app.use('/users', usersRoutes);
 
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../config');
+const Twit = require('twit');
+const T = new Twit(config);
+let userInfo = {};
+const userTweets = [];
+const userFriends = [];
+
+// get friend
+T.get('friends/list', {count: 5}, function(err, data, response)  {
+  data.forEach(function(tweet) {});
+})
+
+// get user information
+T.get('account/verify_credentials', function(err, data, response)  {
+  userInfo = {
+    screen_name: data.screen_name,
+    profile_image: data.profile_image_url
+  }
+})
+
+// get 5 latest tweet
+T.get('statuses/user_timeline', {count: 5},
+  function(err, data, response) {
+    data.forEach(function(tweet) {
+      const tweetObject = {};
+      tweetObject.text = tweet.text;
+      tweetObject.date = tweet.created_at;
+      tweetObject.retweets = tweet.retweet_count;
+      tweetObject.likes = tweet.favorite_count;
+      userTweets.push(tweetObject);
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Twitter Client' });
-  console.log(TwitConfig.consumer_key);
+  res.render('index',
+    { title: 'Twitter Client', userTweets, userInfo }
+  );
 });
 
 module.exports = router;
